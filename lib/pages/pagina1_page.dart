2e509856de93ad5577373fa820dda49f0fab6f39
mari <mari.17ymy@gmail.com>
2020-07-17 21:12:36 -0400

    inicio de proyecto

---
+++ lib/pages/pagina1_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Pagina1Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("kjsdkdsfjkds"),
+    );
+  }
+}
