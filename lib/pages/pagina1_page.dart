891a1cc0947a118524c56895d10d1c9847a80313
mari <mari.17ymy@gmail.com>
2020-07-17 21:48:18 -0400

    diseño pantalla principal

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Pagina1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("kjsdkdsfjkds"),
+      appBar: AppBar(
+        title: Text('Animated'),
+        actions: [
+          IconButton(icon: FaIcon(FontAwesomeIcons.twitter), onPressed: () {  }),
+          IconButton(icon: Icon(Icons.navigate_next) ,onPressed: (){},)
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.new_releases, color: Colors.blue,),
+            Text('Titulo', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w200),),
+            Text('texto pequeño', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w200),),
+            Container(height: 2.0, width: 200.0, color: Colors.blue,)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_circle_filled),
+        onPressed: (){}
+      ),
     );
   }
 }
